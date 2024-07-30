7684f7bf83b9a48a730c4a7b3bf52400e0e812e7
Georjuan Taylor <georjuan@gmail.com>
2024-07-30 19:03:13 -0300

    v1.0.1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
 
   appProperties: {
-    version: 'v1.0.0',
-    releaseDate: '00/00/2024',
+    version: 'v1.0.1',
+    releaseDate: '30/07/2024',
     title: 'Minha Barra - Calculadora de Pesos',
     contactEmail: 'georjuan@gmail.com',
     contactDefaultSubject: 'Contato App - Minha Barra',
